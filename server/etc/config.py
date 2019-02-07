c7bb3ccac54ee5969909af007f444feb8787f57c
Alessio Cecchin <acecchin@gmail.com>
2019-02-07 15:09:38 +0000

    Hide any secret data away from the project

--- server/etc/config.py
+++ server/etc/config.py
@@ -1,11 +1,19 @@
+try:
+    from configparser import ConfigParser
+except ImportError:
+    from ConfigParser import ConfigParser  # ver. < 3.0
+import os.path
+
+
 class Config:
-    def __init__(self):
-        raise Exception('Use this class as static placeholder')
+    safeIniSetup = '/etc/bhash.conf'
 
     #
-    # CHANGE ME! You can use https://ddg.gg/?q=password+strong+32
+    # CHANGE ME!
+    # default values will be overridden from Config.safeIniSetup
+    # You can use https://ddg.gg/?q=password+strong+32
     #
-    secret = 'PWFjbh#Xd9vVgYHQf&Go5D5qJ$*ZrXz3'
+    secret = 'this-seed-add-noise-on-session-keys'
 
     urlPrefix = ''
     sessionLength = 3600
@@ -21,10 +29,14 @@ class Config:
 
     gravatarDefault = 'robohash'
 
+    def __init__(self):
+        raise Exception('Use this class as static placeholder')
+
 
 class DBConfig(Config):
+    # default values will be overridden from Config.safeIniSetup
     user = 'bhash'
-    password = "bHasher"
+    password = "CHANGEME"
     database = 'bhash'
     host = '127.0.0.1'
 
@@ -32,6 +44,20 @@ class DBConfig(Config):
     # CHANGE ME before adding users!
     # You can use https://ddg.gg/?q=password+strong+32
     #
-    seed = 'B6NMyYLnDiwLfKAAp)mN$Syh'
+    seed = 'this-seed-add-noise-on-password-storage'
 
     securityResultLimit = 1000
+
+
+if os.path.isfile(Config.safeIniSetup):
+    config = ConfigParser()
+
+    config.read(Config.safeIniSetup)
+
+    Config.secret = config.get('seeds', 'session')
+    DBConfig.seed = config.get('seeds', 'auth')
+
+    DBConfig.user = config.get('db', 'user')
+    DBConfig.password = config.get('db', 'password')
+    DBConfig.name = config.get('db', 'name')
+    DBConfig.host = config.get('db', 'host')
